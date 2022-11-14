4def039ed575bb467e70e0848a59c6a61498559f
PrfsJ <19522476@gm.uit.edu.vn>
2022-11-14 22:51:10 +0700

    add homework 3

---
+++ Homework/Week2/Homework3/template-processor.js
@@ -0,0 +1,29 @@
+var varDeclared = ['varDeclared', 'p1Message', 'p2Message', 'p3Message'];
+
+class TemplateProcessor{
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(dictionary) {
+        var temp = this.template;
+        for (var key in dictionary) {
+            var re = new RegExp('\\{\\{' + key + '\\}\\}');
+            temp = temp.replace(re, dictionary[key]);
+        }
+        temp = temp.replace(new RegExp('\\{\\{\\w+\\}\\}', "g"), "");
+        return temp;
+    }
+}
+
+var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
+var dateTemplate = new TemplateProcessor(template);
+
+var dictionary = {month: 'July', day: '1', year: '2016'};
+var str = dateTemplate.fillIn(dictionary);
+console.log(str);
+
+// Case: property doesn't exist in dictionary
+var dictionary2 = {day: '1', year: '2016'};
+
+var str = dateTemplate.fillIn(dictionary2);
+console.log(str);
